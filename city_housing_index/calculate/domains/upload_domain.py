3ba7cf30662ae9004f06ac5ab8589913c65a203a
Hammer Sara <3440225591@qq.com>
2021-05-09 01:13:54 +0800

    id -> code

--- city_housing_index/calculate/domains/upload_domain.py
+++ city_housing_index/calculate/domains/upload_domain.py
@@ -35,8 +35,9 @@ def list_upload_files(user):
     data_file_queryset = DataFile.objects.filter(user=user, deleted=False)
     raw_data = DataFileSerializer(data_file_queryset, many=True).data
 
-    city_id_list = [item["city_id"] for item in raw_data]
-    city_queryset = City.objects.filter(id__in=city_id_list)
+    city_id_list = [str(item["city_id"]) for item in raw_data]
+    city_queryset = City.objects.filter(code__in=city_id_list)
+    # city_queryset = City.objects.filter(id__in=city_id_list)
 
     city_data_list = CitySerializer(city_queryset, many=True).data
 
