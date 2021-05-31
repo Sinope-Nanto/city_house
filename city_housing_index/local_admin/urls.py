63fed0bb434682d89b01c36941b61d7056fe2282
jinzixiang1 <jinzixiang1@xuetangx.com>
2021-05-31 22:31:26 +0800

    修改已上传文件状态

--- city_housing_index/local_admin/urls.py
+++ city_housing_index/local_admin/urls.py
@@ -9,11 +9,11 @@ urlpatterns = [
     path("auth/register_users/<int:user_id>", register_view.GetWaitingRegisterUserDetailView.as_view()),
     path("auth/register_users/accept/<int:user_id>", register_view.AcceptRegisterUserView.as_view()),
     path("auth/register_users/refuse/<int:user_id>", register_view.RefuseRegisterUserView.as_view()),
-    path("upload_status",status_view.UploadStatusViews.as_view()),
-    path("upload_files",files_views.UpLoadFilesViews.as_view()),
-    path("download_files/<str:filenames>",download_views.DownLoadViews.as_view()),
-    path("upload_files/download/<int:fileid>",download_views.DownLoadbyIDViews.as_view()),
-    path("upload_files/download_time",download_views.DownLoadbyTime.as_view()),
-    path("upload_files/download_city",download_views.DownLoadbyCity.as_view()),
-    path("upload_files/download_citytime",download_views.DownLoadbyCityTime.as_view())
+    path("upload_status", status_view.UploadStatusViews.as_view()),
+    path("upload_files", files_views.UpLoadFilesViews.as_view()),
+    path("download_files/<str:filenames>", download_views.DownLoadViews.as_view()),
+    path("upload_files/download/<int:fileid>", download_views.DownLoadbyIDViews.as_view()),
+    path("upload_files/download_time", download_views.DownLoadbyTime.as_view()),
+    path("upload_files/download_city", download_views.DownLoadbyCity.as_view()),
+    path("upload_files/download_citytime", download_views.DownLoadbyCityTime.as_view())
 ]
--- city_housing_index/local_admin/views/status_views.py
+++ city_housing_index/local_admin/views/status_views.py
@@ -8,11 +8,12 @@ from utils.api_response import APIResponse
 from local_auth.authentication import CityIndexAuthentication
 from local_admin.permissions import CityIndexAdminPermission
 
+
 def GetUploadStatus():
     text = City.objects.all()
     data_list = []
     for i in text:
-        upload = DataFile.objects.filter(city_id = i.code)
+        upload = DataFile.objects.filter(city_id=i.id)
         exist = False
         for j in upload:
             data = j.start.split('-')
@@ -21,12 +22,14 @@ def GetUploadStatus():
             if int(data[0]) == datetime.datetime.now().year and int(data[1]) == datetime.datetime.now().month:
                 exist = True
                 break
-        data_list.append({'id':i.code,'name':i.name,'upload_status':exist})
+        data_list.append({'id': i.code, 'name': i.name, 'upload_status': exist})
     return data_list
+
+
 class UploadStatusViews(APIView):
     # authentication_classes = [CityIndexAuthentication]
     # permission_classes = [CityIndexAdminPermission]
-    def get(self,request):
+    def get(self, request):
         data = []
         data = GetUploadStatus()
         return APIResponse.create_success(data=data)
