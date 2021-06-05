54d92271ddfe652d7d3fb0a3b86e0946ba3ff395
jinzixiang1 <jinzixiang1@xuetangx.com>
2021-06-05 23:05:53 +0800

    修复url问题

--- city_housing_index/local_auth/urls.py
+++ city_housing_index/local_auth/urls.py
@@ -1,8 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from .views import SendSMSCodeView, LoginView, RegisterView
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'register', RegisterView)
 
 urlpatterns = [
+    path("", include(router.urls)),
     path("get_code/", SendSMSCodeView.as_view()),
     path("login/", LoginView.as_view()),
-    path("register/", RegisterView.as_view())
 ]
