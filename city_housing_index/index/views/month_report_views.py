e4c8c8ec230525649e86e3d507be579ebfc8bfc3
jinzixiang <jinzixiang@baicizhan.com>
2021-06-13 16:14:47 +0800

    develop：防止重复发起多次生成报告

--- city_housing_index/index/views/month_report_views.py
+++ city_housing_index/index/views/month_report_views.py
@@ -16,18 +16,24 @@ class GenReportViews(APIView):
         year = int(request.data['year'])
         month = int(request.data['month'])
         user = request.user
+        running_task = GenReportTaskRecord.objects.filter(kwargs={"year": year, "month": month},
+                                                          finished=False).first()
+        if running_task:
+            result = {
+                "task_id": running_task.id
+            }
+        else:
+            task_record = GenReportTaskRecord(kwargs={"year": year, "month": month})
+            task_record.code = task_record.generate_code()
+            task_record.save()
 
-        task_record = GenReportTaskRecord(kwargs={"year": year, "month": month})
-        task_record.code = task_record.generate_code()
-        task_record.save()
+            print("start delay")
+            generate_report.delay(year=year, month=month, task_id=task_record.id)
 
-        print("start delay")
-        generate_report.delay(year=year, month=month, task_id=task_record.id)
-
-        print("delay over")
-        result = {
-            "task_id": task_record.id
-        }
+            print("delay over")
+            result = {
+                "task_id": task_record.id
+            }
         return APIResponse.create_success(result)
 
 
